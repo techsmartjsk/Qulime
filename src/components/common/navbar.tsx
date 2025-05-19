de387bd78ae0072222422416ac84e5b8097f3ca2
Jaskirat Singh <techsmartjsk@gmail.com>
2025-05-20 00:56:14 +0530

    removed comments

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -7,7 +7,6 @@ import logo from "@/assets/logo.png";
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  // Lock/unlock body scroll based on menu state
   useEffect(() => {
     if (isOpen) {
       document.body.classList.add("overflow-hidden");
@@ -25,7 +24,6 @@ export const Navbar = () => {
       <div className="flex justify-between items-center px-5 lg:px-20">
         <Image src={logo} alt="Qulime Aesthetics" className='w-[250px] lg:w-[400px] lg:mx-auto' />
 
-        {/* Mobile Menu Icon */}
         <div className="lg:hidden z-50">
           <button onClick={toggleMenu}>
             {isOpen ? <X size={28} /> : <Menu size={28} />}
@@ -33,7 +31,6 @@ export const Navbar = () => {
         </div>
       </div>
 
-      {/* Desktop Menu */}
       <div className='hidden lg:flex justify-center items-center gap-x-32 py-5 font-istok'>
         <a className='text-lg'>Home</a>
         <a className='text-lg'>About us</a>
@@ -42,7 +39,6 @@ export const Navbar = () => {
         <a className='text-lg text-white bg-[#31533E] px-6 py-3 rounded-2xl'>Book Consultation</a>
       </div>
 
-      {/* Mobile Menu Overlay */}
       <div
         className={`fixed top-0 left-0 w-full h-full bg-[#EFEAE6] flex flex-col items-center justify-center gap-y-5 font-istok transition-all duration-300 ease-in-out z-40 ${
           isOpen ? 'opacity-100 visible' : 'opacity-0 invisible'
