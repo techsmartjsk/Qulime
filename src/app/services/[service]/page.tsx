60c8dfe1387c94d80a5a279ca74132e6bfff4e4d
Jaskirat Singh <techsmartjsk@gmail.com>
2025-06-01 17:09:17 +0530

    removed service variable

--- src/app/services/[service]/page.tsx
+++ src/app/services/[service]/page.tsx
@@ -2,7 +2,6 @@
 
 import Footer from "@/components/common/footer";
 import { Navbar } from "@/components/common/navbar";
-import { useParams } from "next/navigation"
 
 export default function ServiceInfo() {
     return (
